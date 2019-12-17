50ceab5b142467f7f476badcbeb8d0f7b9eae565
laishouchao <pasalai@qq.com>
2019-12-17 17:57:56 +0800

    greentoday

--- greenall.py
+++ greenall.py
@@ -8,14 +8,15 @@
 
 import os
 import datetime
+import io
 
 pushtoday = []
 today = datetime.date.today()
 print(today)
 for i in range(1000):
     pushtoday.append("pushtody " + str(today))
-print(pushtoday)
-with open("push.txt", "w", encoding="utf-8") as wp:
+# print(pushtoday)
+with io.open("push.txt", "w", encoding="utf-8") as wp:
     for i in range(1000):
         wp.write(pushtoday[i])
 os.system("git add ./push.txt")
